c2a05c5f30756e7325825c1bb28b0e6cdd71828b
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2023-10-19 21:12:39 -0700

    seperating files

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 import ccxt
-# import pandas as pd
 import time
+from methods.movingAvg import init 
 
 # TODO
 # 1. get correct amount after trade and keep track of how much capital you have
@@ -28,94 +28,20 @@ exchange = ccxt.binanceus({
 symbol = 'SOL/USDT'
 timeframes = [5, 8, 13] # must be in order
 # timeframes = ['7d', '25d', '99d']
-averages = []
 amount = -1 # This is the amount of money I am using in USD
 money = 100.07
 curPrice = -1
-closes = []
 bought = False
-
-###### * sums closing prices of the past x days
-def calcAvg():
-  global closes
-  global averages
-  global timeframes
-  total = 0
-  i = 0
-  cnt = 1
-  averages = []
-  for x in reversed(closes):
-    total += x
-    # print(timeframes, x)
-    if cnt == timeframes[i]:
-      # print(cnt)
-      averages.append(total / timeframes[i])
-      i += 1
-    cnt += 1
-  print(averages)
-    
-
-
-
-
-def start():
-  global closes
-  global timeframs
-  days = exchange.fetchOHLCV(symbol, '1m')
-  i = 0
-  total = 0
-  closes = [day[4] for day in days[-timeframes[(len(timeframes)-1)]:]]
-  # print(closes)
-  calcAvg()
-
-
-
-def init():
-  global averages
-  global bought
-  start()
-  if not bought and averages[0] > averages[2] and averages[1] > averages[2]:
-    if exchange.has['createMarketOrder']:
-      # exchange.createMarketBuyOrder(symbol, amount, params = {})
-      # exchange.fetchBalance(params = {})
-      print('bought')
-      bought = True
-  elif bought and averages[0] < averages[2] and averages[1] < averages[2]:
-      # !need to get amount avialable
-    if exchange.has['createMarketOrder']:
-      # order = exchange.createMarketSellOrder(symbol, params = {})
-      # print(order)
-      # amount = order.amount
-      # print(amount)
-      print('sold')
-      bought = False
-  else:
-    print('didnt buy or sell')
-  
-  balance = exchange.fetchBalance(params = {})
-  print(balance.get('USDT'))
 
 timer = 0
 while(True):
   print('running at time:', timer)
-  init()
+  init(exchange, symbol, timeframes)
   time.sleep(1 * 60)
   timer += 1
   if timer >= 60:
     break
 
-
-# def testBuy():
-#   if exchange.has['createMarketOrder']:
-#     orderBuy = exchange.createMarketBuyOrder(symbol, amount, params = {})
-#     print('buy order: ', orderBuy)
-
-
-# def testSell():
-#   if exchange.has['createMarketOrder']:
-#     print(amount)
-#     orderSell = exchange.createMarketSellOrder(symbol, amount, params = {'useBnb': True})
-#     print('sell order: ', orderSell)
 
 def getPrice():
   global amount
@@ -131,8 +57,3 @@ def getPrice():
   print(amount)
   # amount = round(amount, 2)
   # print(amount)
-  # testSell()
-  # testBuy()
-
-
-# getPrice()
