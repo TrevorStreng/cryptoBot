62e494cedfc667cb30eeeb60c9a9fb8854e3eea2
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2023-10-23 19:49:23 -0700

    reorganizing and going to test balance values

--- methods/movingAvg.py
+++ methods/movingAvg.py
@@ -1,22 +1,42 @@
 bought = False
-def calcAvg(closes, timeframes):
-  total = 0
-  i = 0
-  cnt = 1
-  averages = []
-  for x in reversed(closes):
-    total += x
-    # print(timeframes, x)
-    if cnt == timeframes[i]:
-      # print(cnt)
-      averages.append(total / timeframes[i])
-      i += 1
-    cnt += 1
-  print(averages)
-  return averages
 
+# 1.
+def initMovAvg(exchange, symbol, timeframes):
+  global bought
+  symbols = createSymbols(symbol)
+  averages = start(exchange, symbol, timeframes)
+  # bal = getBalance(exchange, symbols[1])
+  # print(bal)
+  if not bought and averages[0] > averages[2] and averages[1] > averages[2]:
+    if exchange.has['createMarketOrder']:
+      bal0 = getBalance(exchange, symbols[1]) # ^ need to get the amount of usdt to see how much SOL to buy
+      print(bal0.free)
+      print(type(bal0.free))
+      # order = exchange.createMarketBuyOrder(symbol, bal0.free, params = {})
+      # print(order)
+      print('bought')
+      bought = True
+  elif bought and averages[0] < averages[2] and averages[1] < averages[2]:
+    if exchange.has['createMarketOrder']:
+      bal1 = getBalance(exchange, symbols[0]).get(symbols[1]) # ^ need to get the amount of SOL to see how much to sell
+      # order = exchange.createMarketSellOrder(symbol, bal1, params = {})
+      print(bal1.free)
+      print(type(bal1.free))
+      print('sold')
+      bought = False
+  else:
+    print('didnt buy or sell')
+
+# 2.
+def createSymbols(symbol):
+  return symbol.split('/') # 'SOL/USDT' returns array
+
+# 3.
+def getBalance(exchange, symbol):
+  return exchange.fetchBalance(params={}).get(symbol)
+
+# 4.
 def start(exchange, symbol, timeframes):
-  # global closes
   global timeframs
   days = exchange.fetchOHLCV(symbol, '1m')
   i = 0
@@ -25,35 +45,18 @@ def start(exchange, symbol, timeframes):
   print(closes)
   return calcAvg(closes, timeframes)
 
-def initMovAvg(exchange, symbol, timeframes):
-  global bought
-  symbols = createSymbols(symbol)
-  averages = start(exchange, symbol, timeframes)
-  bal = getBalance(exchange, symbols[1])
-  print(bal)
-  if not bought and averages[0] > averages[2] and averages[1] > averages[2]:
-    if exchange.has['createMarketOrder']:
-      bal0 = getBalance(exchange, symbols[1]) # ^ need to get the amount of usdt to see how much SOL to buy
-      print(bal0) # !!!! this prints none
-      # order = exchange.createMarketBuyOrder(symbol, bal0, params = {})
-      # print(order)
-      print('bought')
-      bought = True
-  elif bought and averages[0] < averages[2] and averages[1] < averages[2]:
-    if exchange.has['createMarketOrder']:
-      bal1 = getBalance(exchange, symbols[0]).get(symbols[1]) # ^ need to get the amount of SOL to see how much to sell
-      # order = exchange.createMarketSellOrder(symbol, bal1, params = {})
-      print(bal1)
-      print('sold')
-      bought = False
-  else:
-    print('didnt buy or sell')
+# 5.
+def calcAvg(closes, timeframes):
+  total = 0
+  i = 0
+  cnt = 1
+  averages = []
+  for x in reversed(closes):
+    total += x
+    if cnt == timeframes[i]:
+      averages.append(total / timeframes[i])
+      i += 1
+    cnt += 1
+  print(averages)
+  return averages
   
-  # balance = exchange.fetchBalance(params = {}).get(symbols[0])
-  # print(balance.get('USDT'))
-
-def createSymbols(symbol):
-  return symbol.split('/') # 'SOL/USDT' returns array
-
-def getBalance(exchange, symbol):
-  return exchange.fetchBalance(params={}).get(symbol)
