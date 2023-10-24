6d253616fb7c2dc95bfac16f1394f85d6d701a3a
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2023-10-23 20:10:04 -0700

    back to testing

--- methods/movingAvg.py
+++ methods/movingAvg.py
@@ -5,14 +5,15 @@ def initMovAvg(exchange, symbol, timeframes):
   global bought
   symbols = createSymbols(symbol)
   averages = start(exchange, symbol, timeframes)
-  # bal = getBalance(exchange, symbols[1])
-  # print(bal.get('free'))
   if not bought and averages[0] > averages[2] and averages[1] > averages[2]:
     if exchange.has['createMarketOrder']:
       bal0 = getBalance(exchange, symbols[1]) # ^ need to get the amount of usdt to see how much SOL to buy
       # ! need to calculate the amount of SOL to buy, might be tough with market orders
-
-      # order = exchange.createMarketBuyOrder(symbol, bal0.get('free'), params = {})
+      orderbook = exchange.fetch_order_book (exchange.symbols[0])
+      ask = orderbook['asks'][0][0] if len (orderbook['asks']) > 0 else None
+      amount = bal0.get('free') / ask # amount that I want to buy
+      print('amount: ', amount)
+      order = exchange.createLimitBuyOrder(symbol, amount, ask, params = {})
       print(order)
       print('bought')
       bought = True
@@ -20,7 +21,7 @@ def initMovAvg(exchange, symbol, timeframes):
     if exchange.has['createMarketOrder']:
       bal1 = getBalance(exchange, symbols[0]).get(symbols[1]) # ^ need to get the amount of SOL to see how much to sell
       # order = exchange.createMarketSellOrder(symbol, bal1.get('free'), params = {})
-      print(order)
+      # print(order)
       print('sold')
       bought = False
   else:
