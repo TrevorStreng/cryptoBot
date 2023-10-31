d2df15eed4839692418c1d824d894b6d6d8ac401
trevorstreng <trevorstreng@csus.edu>
2023-10-30 21:16:14 -0700

    still getting error

--- methods/movingAvg.py
+++ methods/movingAvg.py
@@ -1,4 +1,23 @@
 # 1.
+# ! Still gettng precision error
+# amount: %s 0.0057042493744787325
+# Traceback (most recent call last):
+#   File "index.py", line 62, in <module>
+#     startTrading()
+#   File "index.py", line 52, in startTrading
+#     initMovAvg(exchange, symbol, timeframes, logging, bought)
+#   File "/home/pi/Documents/crypto_bot/cryptoBot/methods/movingAvg.py", line 14, in initMovAvg
+#     order = exchange.createLimitBuyOrder(symbol, amount, ask, params = {})
+#   File "/home/pi/.local/lib/python3.7/site-packages/ccxt/base/exchange.py", line 3640, in create_limit_buy_order
+#     return self.create_order(symbol, 'limit', 'buy', amount, price, params)
+#   File "/home/pi/.local/lib/python3.7/site-packages/ccxt/binance.py", line 4174, in create_order
+#     request = self.create_order_request(symbol, type, side, amount, price, params)
+#   File "/home/pi/.local/lib/python3.7/site-packages/ccxt/binance.py", line 4352, in create_order_request
+#     request['quantity'] = self.amount_to_precision(symbol, amount)
+#   File "/home/pi/.local/lib/python3.7/site-packages/ccxt/base/exchange.py", line 3666, in amount_to_precision
+#     raise InvalidOrder(self.id + ' amount of ' + market['symbol'] + ' must be greater than minimum amount precision of ' + self.number_to_string(market['precision']['amount']))
+# ccxt.base.errors.InvalidOrder: binanceus amount of SOL/USDT must be greater than minimum amount precision of 2
+
 def initMovAvg(exchange, symbol, timeframes, logging, bought):
   symbols = createSymbols(symbol)
   averages = start(exchange, symbol, timeframes)
@@ -6,13 +25,13 @@ def initMovAvg(exchange, symbol, timeframes, logging, bought):
     if exchange.has['createMarketOrder']:
       bal1 = getBalance(exchange, symbols[1]) # ^ need to get the amount of usdt to see how much SOL to buy
       orderbook = exchange.fetch_order_book (symbol)
-      print(orderbook)
+      # print(orderbook)
       ask = orderbook['asks'][0][0] if len (orderbook['asks']) > 0 else None
-      print(ask)
+      # print(ask)
       amount = bal1 / ask # amount that I want to buy
       print('amount: %s', amount)
       order = exchange.createLimitBuyOrder(symbol, amount, ask, params = {})
-      print(order)
+      # print(order)
       bought = True
       logging.info('Bought: ', order) # & Make sure to log price and amount bought
   elif bought and averages[0] < averages[2] and averages[1] < averages[2]:
